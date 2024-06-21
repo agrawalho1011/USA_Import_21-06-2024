7f821a96c4c1c7ed50bea9252a8334a0b9942374
agrawalho1011 <125637960+agrawalho1011@users.noreply.github.com>
2024-06-21 12:00:30 +0530

    USA Import Updated on 21-06-2024
    USA Import Updated on 21-06-2024

---
+++ SQLQuery/Export_WorkflowWeb Query.sql
@@ -0,0 +1,12 @@
+﻿select * from HBLMaster
+select hm.HBLNumber,am.NameOfActivity,hm.Booking,fm.Etd,sm.[Status],ha.Comment,ha.StartDate,ha.EndDate,um.UserName from HBLActivityLog as Ha
+inner join ActivityMaster as am
+on ha.ActivityId=am.Id
+inner join StatusMaster as sm
+on ha.StatusId=sm.Id
+inner join HBLMaster as hm
+on ha.HBLId=hm.Id
+inner join AspNetUsers as um
+on ha.UserId=um.Id
+inner join FileMaster as fm
+on fm.Id=hm.FileId
